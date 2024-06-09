8e972d09ff102a78e0d99b513869c8c9fee2c76e
rpjcode <69083318+rpjdcode@users.noreply.github.com>
2024-06-09 16:38:34 +0100

    Dependencia commons en MicroReactivo

--- micro-reactivo-prueba/src/main/java/com/eviden/microreactivo/MicroReactivoPruebaApplication.java
+++ micro-reactivo-prueba/src/main/java/com/eviden/microreactivo/MicroReactivoPruebaApplication.java
@@ -3,7 +3,7 @@ package com.eviden.microreactivo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.eviden.commons.eureka", "com.eviden.microreactivo"})
 public class MicroReactivoPruebaApplication {
 
 	public static void main(String[] args) {
---
+++ micros-common/README.md
@@ -0,0 +1,6 @@
+**micros-common**
+
+El objetivo de este proyecto es evitar el uso de código repetitivo en los microservicios, garantizando un desarrollo limpio e independiente en cada microservicio.
+
+Este proyecto es compilado y se utiliza como una librería en los proyectos que requieran alguna de las funcionalidades que se implementan en él.
+
